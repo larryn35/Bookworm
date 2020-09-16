6b286f5ce4ff44887550896dae143c64a82634a7
Larry N <larryn35@gmail.com>
2020-09-15 22:05:40 -0700

    save book review

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -11,51 +11,28 @@ import SwiftUI
 struct ContentView: View {
     
     @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Book.entity(), sortDescriptors: []) var books: FetchedResults<Book>
     
-    @State private var title = ""
-    @State private var author = ""
-    @State private var rating = 3
-    @State private var genre = ""
-    @State private var review = ""
-
-    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+    @State private var showingAddScreen = false
     
     var body: some View {
         NavigationView {
-            Form {
-                Section {
-                    TextField("Name of book", text: $title)
-                    TextField("Author's name", text: $author)
-                    
-                    Picker("Genre", selection: $genre) {
-                        ForEach(genres, id:\.self) {
-                            Text($0)
-                        }
-                    }
-                }
-                
-                Section {
-                    Picker("Rating", selection: $rating) {
-                        ForEach(0..<6) {
-                            Text("\($0)")
-                        }
-                    }
-                    TextField("Write a review", text: $review)
-                }
-                
-                Section {
-                    Button("Save") {
-                        // add book
-                    }
-                }
+            Text("Count: \(books.count)")
+                .navigationBarTitle("Bookworm")
+                .navigationBarItems(trailing: Button(action: {
+                    self.showingAddScreen.toggle()
+                }) {
+                        Image(systemName: "plus")
+                    })
+                .sheet(isPresented: $showingAddScreen) {
+                    AddBookView().environment(\.managedObjectContext, self.moc)
             }
-        .navigationBarTitle("Add Book")
         }
     }
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
 }
